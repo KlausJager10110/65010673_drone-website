416b1f96f7ede7a8ceeb7160befa1def7b40f789
Chorn <pacharapolpacharapol2547@gmail.com>
2024-10-24 20:13:12 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const { readdirSync } = require('fs');
 const cors = require('cors')
+const path = require('path');
 
 // Declare variable Zone
 const server = express();
@@ -11,7 +12,7 @@ server.use(express.json());
 server.use(cors());
 
 // Automatically generate routes
-readdirSync("./Routes").map((r) => server.use(require("./Routes/" + r)));
+readdirSync(path.join(__dirname, 'Routes')).map((r) => server.use(require("./Routes/" + r)));
 // readdirSync("./Routes").map((r) => server.use("/api", require("./Routes/" + r)));
 
 server.listen(PORT, async () => {
